a98d9a1cc74c9b57fd0fa95c648d6f705b55c01d
a12a1 <a12a12a12a@mail.ru>
2018-09-14 16:18:38 +0300

    1

--- src/ru/innopolis/home_work_4/MyMainMethod.java
+++ src/ru/innopolis/home_work_4/MyMainMethod.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class MyMainMethod {
+    /////
     public static void main(String[] args) {
         String[] words = new String[]{"aaaaaaaaaa", "bbbbbbbbbb", "cccccccccc"};
         getFile("d://", 5, 50, words,1);
