a72bd42c4b47e5025a1e055b560f4aed8d820307
a12a1 <a12a12a12a@mail.ru>
2018-09-15 11:10:41 +0300

    1

---
+++ src/ru/innopolis/home_work_4/SingleParserFile.java
@@ -0,0 +1,19 @@
+package ru.innopolis.home_work_4;
+
+public class SingleParserFile extends Thread {
+
+    /*private String result;
+    private String fileName;
+    private String[] files;
+
+    public SingleParserFile(String result, String fileName, String[] files){
+        this.result = result;
+        this.fileName = fileName;
+        this.files = files;
+    }*/
+
+    @Override
+    public void run() {
+
+    }
+}
