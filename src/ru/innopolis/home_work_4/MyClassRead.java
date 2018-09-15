86c51c84052bebf408b79ec43066948080a2cdb3
a12a1 <a12a12a12a@mail.ru>
2018-09-15 12:31:26 +0300

    1

--- src/ru/innopolis/home_work_4/MyClassRead.java
+++ src/ru/innopolis/home_work_4/MyClassRead.java
@@ -1,94 +1,61 @@
 package ru.innopolis.home_work_4;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class MyClassRead {
 
+    private String itogString = "";
+    private List<Thread> threads = new ArrayList<>();
+
     public void getOccurencies(String[] strPath, String[] strWord, String pathWrite) {
-        try (FileOutputStream writer = new FileOutputStream(pathWrite)) {
+        /*try (FileOutputStream writer = new FileOutputStream(pathWrite)) {
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
+
+        String qwert = "";
         for (String i : strPath) {
-            write(i, strWord, pathWrite);
+            SingleParserFile thread = new SingleParserFile(i, strWord, itogString);
+            thread.start();
+            qwert = thread.getItogString();
+            threads.add(thread);
         }
-    }
 
-    private static String read(String pathRead) {
-        String string = "";
-        try(FileInputStream fileInputStream = new FileInputStream(pathRead)){
-            int i;
-            while((i = fileInputStream.read()) != -1){
-                string += (char) i;
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return string;
-    }
-
-    private static String find(String pathRead, String[] strWord) {
-        String string = read(pathRead).replace("\r\n","")
-                .replace(".", ".___")
-                .replace("!", "!___")
-                .replace("?", "?___");
-        ArrayList<String> arrayList = new ArrayList<>();
-        for(String i:string.split("___ ")){
-            arrayList.add(i);
-        }
-        ArrayList<String> arrayList2 = new ArrayList<>();
-        for(String i:strWord){
-            arrayList2.add(i);
-        }
-        String string2 = "";
-        for(String i:arrayList){
-            for(String j:arrayList2){
-                if ((i.contains(" " + j + " ")) ||
-                        (i.contains(" " + j + ".")) ||
-                        (i.contains(" " + j + "!")) ||
-                        (i.contains(" " + j + "?")) ||
-                        (i.contains(j.substring(0, 1).toUpperCase() + j.substring(1) + " ")) ||
-                        (i.contains(j.substring(0, 1).toUpperCase() + j.substring(1) + ".")) ||
-                        (i.contains(j.substring(0, 1).toUpperCase() + j.substring(1) + "!")) ||
-                        (i.contains(j.substring(0, 1).toUpperCase() + j.substring(1) + "?"))) {
-                    string2 += (i + " ");
-                }
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
-        return string2;
+
+        System.out.println(qwert);
+        write(pathWrite);
     }
 
-    private static void write(String pathRead, String[] strWord, String pathWrite) {
-        String string = "";
-        try(FileInputStream fileInputStream = new FileInputStream(pathWrite)){
-            int i;
-            while((i = fileInputStream.read()) != -1){
-                string += (char) i;
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+    private void write(String pathWrite) {
         try(FileOutputStream fileOutputStream = new FileOutputStream(pathWrite)){
-            String strItog = string + find(pathRead, strWord);
-            byte[] buffer = strItog.getBytes();
+            byte[] buffer = itogString.getBytes();
             fileOutputStream.write(buffer, 0, buffer.length);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        /*try(BufferedWriter writer = new BufferedWriter(new FileWriter(pathWrite))) {
+            writer.write(itogString);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
     }
 }
--- src/ru/innopolis/home_work_4/SingleParserFile.java
+++ src/ru/innopolis/home_work_4/SingleParserFile.java
@@ -1,19 +1,77 @@
 package ru.innopolis.home_work_4;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class SingleParserFile extends Thread {
 
-    /*private String result;
-    private String fileName;
-    private String[] files;
+    private String pathRead;
+    private String[] strWord;
+    private String itogString;
 
-    public SingleParserFile(String result, String fileName, String[] files){
-        this.result = result;
-        this.fileName = fileName;
-        this.files = files;
-    }*/
+    public SingleParserFile(String pathRead, String[] strWord, String itogString) {
+        this.pathRead = pathRead;
+        this.strWord = strWord;
+        this.itogString = itogString;
+    }
+
+    public String getItogString() {
+        return itogString;
+    }
 
     @Override
     public void run() {
+        find(pathRead, strWord);
+        System.out.println(getItogString());
+    }
 
+    private String read(String pathRead) {
+        String string = "";
+        try (FileInputStream fileInputStream = new FileInputStream(pathRead)) {
+            int i;
+            while ((i = fileInputStream.read()) != -1) {
+                string += (char) i;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return string;
+    }
+
+    private void find(String pathRead, String[] strWord) {
+        String string = read(pathRead).replace("\r\n", "")
+                .replace(".", ".___")
+                .replace("!", "!___")
+                .replace("?", "?___");
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (String i : string.split("___ ")) {
+            arrayList.add(i);
+        }
+        ArrayList<String> arrayList2 = new ArrayList<>();
+        for (String i : strWord) {
+            arrayList2.add(i);
+        }
+        String string2 = "";
+        for (String i : arrayList) {
+            for (String j : arrayList2) {
+                if ((i.contains(" " + j + " ")) ||
+                        (i.contains(" " + j + ".")) ||
+                        (i.contains(" " + j + "!")) ||
+                        (i.contains(" " + j + "?")) ||
+                        (i.contains(j.substring(0, 1).toUpperCase() + j.substring(1) + " ")) ||
+                        (i.contains(j.substring(0, 1).toUpperCase() + j.substring(1) + ".")) ||
+                        (i.contains(j.substring(0, 1).toUpperCase() + j.substring(1) + "!")) ||
+                        (i.contains(j.substring(0, 1).toUpperCase() + j.substring(1) + "?"))) {
+                    string2 += (i + " ");
+                }
+            }
+        }
+        synchronized (itogString) {
+            itogString += string2;
+        }
     }
 }
