59be7d95c62d619d32faa2da6ade9c26d19e6724
a12a1 <a12a12a12a@mail.ru>
2018-09-20 15:19:57 +0300

    1

--- src/ru/innopolis/home_work_4/Main.java
+++ src/ru/innopolis/home_work_4/Main.java
@@ -4,17 +4,18 @@ import java.net.URI;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        /*Long startTime1 = System.currentTimeMillis();
+        Long startTime1 = System.currentTimeMillis();
         ClassGenerateText classGenerateText = new ClassGenerateText();
         String[] words = new String[]{"aaaaaaaaaa", "bbbbbbbbbb", "cccccccccc"};
         classGenerateText.getFile("d://", 5, 50000, words, 1);
-        System.out.println("Время записи файлов: " + (System.currentTimeMillis() - startTime1));*/
+        System.out.println("Время записи файлов: " + (System.currentTimeMillis() - startTime1));
 
         System.out.println("1:" + System.currentTimeMillis());
         Long startTime2 = System.currentTimeMillis();
         ClassGererateNewTextFile classGererateNewTextFile = new ClassGererateNewTextFile();
-        //URI[] strPath = {URI.create("d://file1.txt")};
+        //String[] strWord = {"bbbbbbbbbb", "cccccccccc"};
         String[] strWord = {"bbbbbbbbbb"};
+        //URI[] strPath = {URI.create("d://file1.txt")};
         URI[] uri = {URI.create("d://file1.txt"), URI.create("d://file2.txt"),
                 URI.create("d://file3.txt"), URI.create("d://file4.txt"), URI.create("d://file5.txt")};
         classGererateNewTextFile.getOccurencies(uri, strWord, "d://Itog.txt", 5);
